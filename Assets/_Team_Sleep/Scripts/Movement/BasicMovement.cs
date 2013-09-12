f48a43d07da209a47219dce993ebb82c574ceb8a
Newman Willis <newmanwillis@gmail.com>
2013-09-12 07:51:11 -0500

    Basic file organization and game setup

---
+++ Assets/_Team_Sleep/Scripts/Movement/BasicMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicMovement : MonoBehaviour {
+	
+	float Speed = 0.5f;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void FixedUpdate(){
+		
+		Vector3 curPos = transform.position;
+		if(Input.GetKey(KeyCode.W)){
+			curPos.y += Speed;
+		}		
+		if(Input.GetKey(KeyCode.S)){
+			curPos.y -= Speed;
+		}
+		if(Input.GetKey(KeyCode.D)){
+			curPos.x += Speed;
+		}		
+		if(Input.GetKey(KeyCode.A)){
+			curPos.x -= Speed;
+		}			
+		transform.position = curPos;
+	}
+}
