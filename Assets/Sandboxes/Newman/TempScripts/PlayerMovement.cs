14a78dbb466d8b5450c0ff8d35adc5c1296f5ed5
Newman Willis <newmanwillis@gmail.com>
2013-09-17 14:40:46 -0500

    Zombie and Player interaction partly done

---
+++ Assets/Sandboxes/Newman/TempScripts/PlayerMovement.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	
+	public float Speed = 0.5f;
+	public Transform swordAttack;
+
+	float attackAngle = 0f;	
+	private bool isAttacking = false;
+	//Transform swordAttacking;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space) && !isAttacking){
+			isAttacking = true;
+			if(swordAttack){
+
+				Transform swordAttacking = (Transform)Instantiate(swordAttack, transform.position, Quaternion.identity);
+				swordAttacking.Rotate(0,0,attackAngle);
+				swordAttacking.parent = transform;
+				StartCoroutine( FinishAttackAnimation(swordAttacking));
+				
+				
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector3 curPos = transform.position;
+		if(Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D)){
+			curPos.y += Speed;
+			curPos.x += Speed;
+			attackAngle = 315;
+		}
+		else if(Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A)){
+			curPos.y += Speed;
+			curPos.x -= Speed;
+			attackAngle = 45;
+		}
+		else if(Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D)){
+			curPos.y -= Speed;
+			curPos.x += Speed;
+			attackAngle = 225;
+		}	
+		else if(Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A)){
+			curPos.y -= Speed;
+			curPos.x -= Speed;
+			attackAngle = 135;
+		}			
+		else if(Input.GetKey(KeyCode.W)){
+			curPos.y += Speed;
+			attackAngle = 0;
+		}		
+		else if(Input.GetKey(KeyCode.S)){
+			curPos.y -= Speed;
+			attackAngle = 180;
+		}
+		else if(Input.GetKey(KeyCode.D)){
+			curPos.x += Speed;
+			attackAngle = 270;
+		}		
+		else if(Input.GetKey(KeyCode.A)){
+			curPos.x -= Speed;
+			attackAngle = 90;			
+		}	
+	
+		transform.position = curPos;
+	}
+
+	IEnumerator FinishAttackAnimation(Transform sword){
+		yield return new WaitForSeconds(0.7f);
+		//swordAttacking = null;
+		Destroy(sword.gameObject);
+		isAttacking = false;
+	}
+	
+	
+}
---
+++ Assets/Sandboxes/Newman/TempScripts/ZombieFollowPlayer.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieFollowPlayer : MonoBehaviour {
+	
+	public float moveSpeed = 0.2f;
+	public float attackRange = 1.5f;
+	bool foundPlayer = false;
+	Transform player;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void FixedUpdate(){
+		if(foundPlayer){
+			Vector3 newPos = transform.position;
+			Vector3 distance = transform.position - player.transform.position;
+			if(distance.x > moveSpeed)
+				newPos.x -= moveSpeed;
+			else if(distance.x < -moveSpeed)
+				newPos.x += moveSpeed;
+			if(distance.y > moveSpeed)
+				newPos.y -= moveSpeed;
+			else if(distance.y < -moveSpeed)
+				newPos.y += moveSpeed;			
+			
+			transform.position = newPos;
+		}
+	}
+	
+	void OnTriggerEnter(Collider other){
+		Debug.Log("In Colliderr Enter");
+		print ("in collider");
+		if(other.tag == "Player"){
+			player = other.transform;
+			foundPlayer = true;
+		}
+	}
+}
