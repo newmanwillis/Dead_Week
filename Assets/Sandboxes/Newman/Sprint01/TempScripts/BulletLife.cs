37545f28adb516697b7832ad81a834584eb07935
Newman Willis <newmanwillis@gmail.com>
2013-09-20 03:45:24 -0500

    merge

---
+++ Assets/Sandboxes/Newman/Sprint01/TempScripts/BulletLife.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletLife : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnTriggerEnter(Collider other){
+		if(other.tag != "Player" && other.tag != "Attack" && other.tag != "ZombieDetectionRange" && other.tag != "ZombieAttack"){
+			Destroy(gameObject);	
+		}
+	}
+}
