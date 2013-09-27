f38d5c354a7c63b5cdd2c5a8fd3d7548f8dc2411
Newman Willis <newmanwillis@gmail.com>
2013-09-27 04:25:11 -0500

    Beginning zombie AI scripts

---
+++ Assets/Sandboxes/Newman/Scripts/Zombie.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Zombie : MonoBehaviour {
+	
+	public enum ZombieState {wandering, chasing, attacking, dying}
+	
+	public float wanderSpeed = 0.25f;
+	public float chaseSpeed = 0.5f;
+	public int health = 100;
+	
+	public ZombieWander zombieWander;
+	
+	public ZombieState curState;
+	
+	// Use this for initialization
+	void Start () {
+		zombieWander = transform.GetComponent<ZombieWander>();
+		
+		curState = ZombieState.wandering;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		switch(curState){
+		case ZombieState.wandering:
+			zombieWander.Wander();
+		break;
+				
+			
+		}
+		
+	}
+}
---
+++ Assets/Sandboxes/Newman/Scripts/ZombieWander.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieWander : MonoBehaviour {
+	
+	public enum wanderState {calculateWaitTime, waiting, wandering}
+	public wanderState curState;
+	
+	private bool calculatedWaitTime = false;
+	private float waitTime;
+	
+	// Use this for initialization
+	void Start () {
+		curState = wanderState.calculateWaitTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public void Wander (){
+		
+		switch(curState){
+		
+		case wanderState.calculateWaitTime:
+			calculateWaitTime();
+			break;
+		case wanderState.waiting:
+			
+			break;
+			
+		}
+		
+		
+		if(!calculatedWaitTime){
+				
+		}
+	}
+	
+	void calculateWaitTime(){
+		waitTime = 0.25f * Random.Range(2, 14);
+		curState = wanderState.waiting;
+	}
+	
+	IEnumerator waiting() {
+		yield return new WaitForSeconds(waitTime);
+	}
+	
+}
