ae5bc5f6509c07a8764f1663f0ba551de010848a
Liam McMahon <lmmcmahon9@gmail.com>
2013-10-06 18:52:05 -0500

    added basic cutscene intro

---
+++ Assets/_Team_Sleep/Cutscenes/CutsceneUtil.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CutsceneUtil : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void printText(string text) {
+		Camera.main.GetComponent<CameraControl>().pauseAndDrawTextMessage(text);
+	}
+	
+	void switchScene(string sceneName) {
+		Application.LoadLevel(sceneName);
+	}
+}
---
+++ Assets/_Team_Sleep/Cutscenes/Intro/PlayerAnimationScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAnimationScript : MonoBehaviour {
+	private tk2dSpriteAnimator curAnim;
+	
+	// Use this for initialization
+	void Start () {
+		curAnim = GetComponentInChildren<tk2dSpriteAnimator>();
+		curAnim.Play("walkingLeft");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void swordAttackAnimation() {
+		curAnim.Play("swordRight");
+		StartCoroutine(waitForAnimationtoEnd());
+	}
+	
+	IEnumerator waitForAnimationtoEnd(){
+		while(curAnim.Playing){
+			yield return null;
+		}
+		// curAnim.Pause();
+		curAnim.Play("walkingRight");
+	}
+}
