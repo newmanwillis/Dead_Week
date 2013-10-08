229b0d167cee44840424fad2ca66eea73f728651
Liam McMahon <lmmcmahon9@gmail.com>
2013-10-08 00:16:21 -0500

    added a first zombie encounter cutscene

---
+++ Assets/_Team_Sleep/Scripts/CutsceneTrigger.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CutsceneTrigger : MonoBehaviour {
+	public bool spawnZombie;
+	public Vector3 spawnPos;
+	public string zombieAnimation;
+	
+	public Transform zombie;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Player") {
+			other.animation.Play("PlayerSpotsZombie");
+			
+			if (spawnZombie) {
+				Transform spawned = (Transform)Instantiate(zombie, spawnPos, Quaternion.identity);
+				spawned.animation.Play("ZombieWalksLeft");
+			}
+			Destroy(this.gameObject);
+		}
+	}
+}
