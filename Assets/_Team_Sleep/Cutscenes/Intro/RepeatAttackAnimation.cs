ba93d5e8448e69613a08e5978298a0dde7b19452
Liam McMahon <lmmcmahon9@gmail.com>
2013-10-08 22:11:30 -0500

    added zombie attacks to cutscene

---
+++ Assets/_Team_Sleep/Cutscenes/Intro/RepeatAttackAnimation.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RepeatAttackAnimation : MonoBehaviour {
+	private tk2dSpriteAnimator animator;
+	private string left = "Left Attack";
+	private string right = "Right Attack";
+	private bool onLeft = true;
+	
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<tk2dSpriteAnimator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!animator.Playing) {
+			if (onLeft) {
+				animator.Play(right);
+			} else {
+				animator.Play(left);
+			}
+			onLeft = !onLeft;
+		}
+	}
+}
