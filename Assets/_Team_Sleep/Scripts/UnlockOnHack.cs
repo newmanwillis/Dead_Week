be86a7e7eff5c9789e9c631638805b75fc0fa03e
Liam McMahon <lmmcmahon9@gmail.com>
2013-10-15 23:07:48 -0500

    added a door to prevent the player from leaving without a terminal

---
+++ Assets/_Team_Sleep/Scripts/UnlockOnHack.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnlockOnHack : MonoBehaviour {
+	
+	private HackableComputer[] computerTerminals;
+	// Use this for initialization
+	void Start () {
+		GameObject[] computers = GameObject.FindGameObjectsWithTag("ComputerTerminal");
+		computerTerminals = new HackableComputer[computers.Length];
+		for (int i = 0; i < computers.Length; i++) {
+			computerTerminals[i] = computers[i].GetComponent<HackableComputer>();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		bool allHacked = true;
+		foreach (HackableComputer comp in computerTerminals) {
+			if (comp.hackSoFar < comp.timeToHackSeconds) {
+				allHacked = false;
+			}
+		}
+		
+		if (allHacked) {
+			Destroy(gameObject);
+		}
+	}
+}
