2437271c3f47bb0cf529d5f503c13e157a4c9e55
Newman Willis <newmanwillis@gmail.com>
2013-10-29 19:37:02 -0500

    None

---
+++ Assets/Sandboxes/Newman/Scripts/ZombieSpawner.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieSpawner : MonoBehaviour {
+
+	// public....direction up/down
+	public float spawnTimer = 5f;
+	private float spawnDelay;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// 1. Check there's nothing inside
+		// 2. Put timer between each zombie spawn
+		// 3. make zombies move desired amount/direction into hallway
+		
+		
+		
+	}
+	
+	void OnTriggerEnter(Collider other){
+		
+	}
+	
+	void OnTriggerStay(Collider other){
+			
+	}
+	
+	void OnTriggerExit(Collider other){
+		
+	}
+	
+}
