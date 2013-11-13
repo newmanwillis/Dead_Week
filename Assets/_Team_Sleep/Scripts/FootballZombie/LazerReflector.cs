6b65d8e4d3bb0221c02531e389a9341a4da07334
Liam McMahon <lmmcmahon9@gmail.com>
2013-11-13 14:18:01 -0600

    the boss's sheild now reflects bullets

---
+++ Assets/_Team_Sleep/Scripts/FootballZombie/LazerReflector.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LazerReflector : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Attack") {
+			Vector3 inDirection = other.rigidbody.velocity;
+			Vector3 reflectAxis = transform.position - other.transform.position;
+			float dot = Vector3.Dot(reflectAxis, inDirection);
+			Vector3 outDirection = inDirection - 2*(dot * reflectAxis);
+			other.transform.position = outDirection;
+			
+			Debug.Log("In : " + inDirection + "\nReflect: " + reflectAxis + "\nOut: " + outDirection);
+		}
+	}
+}
