02d78573574cc650674892cc417060b30c50465f
Liam McMahon <lmmcmahon9@gmail.com>
2013-11-19 00:02:02 -0600

    added wire lights to the first generator level

---
+++ Assets/_Team_Sleep/Scripts/FollowLinearPath.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowLinearPath : MonoBehaviour {
+	private Vector3[] pathPoints;
+	public int index = 0;
+	private float delta = 0.01f;
+	public float speed;
+	public string defaultPathName;
+	// Use this for initialization
+	void Start () {
+		if (defaultPathName != null && defaultPathName != "") {
+			setPath(defaultPathName, false);
+		}
+	}
+
+	public void setPath(string pathName, bool setPosAndIndex = true) {
+		GameObject pathParentObj = GameObject.Find(pathName);
+		int count = pathParentObj.transform.childCount;
+		pathPoints = new Vector3[count];
+		for (int i = 0; i < count; ++i) {
+			pathPoints[i] = pathParentObj.transform.GetChild(i).position;
+		}
+		if (setPosAndIndex) {
+			index = 0;
+			transform.position = pathPoints[0];
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 between = pathPoints[index] - transform.position;
+		if (between.magnitude < delta) {
+			index++;
+			if (index == pathPoints.Length) {
+				Destroy(gameObject);
+				return;
+			}
+		}
+
+		Vector3 dir = between.normalized;
+		Vector3 move = dir * speed * Time.deltaTime;
+		if (move.magnitude > between.magnitude) {
+			move = move.normalized * between.magnitude;
+		}
+		transform.position += move;
+	}
+}
--- Assets/_Team_Sleep/Scripts/RepeatedlySpawn.cs
+++ Assets/_Team_Sleep/Scripts/RepeatedlySpawn.cs
@@ -2,17 +2,26 @@ using UnityEngine;
 using System.Collections;
 
 public class RepeatedlySpawn : MonoBehaviour {
-	public float spawnInterval;
+	public float spawnInterval = 4;
 	private float nextSpawnTime;
 	public Transform transformToSpawn;
+
+	public string pathName;
 	
 	void Start() {
+		nextSpawnTime = 0;
+		if (transformToSpawn == null) {
+			Debug.LogError("Not assigned");
+			transformToSpawn = ((GameObject) Resources.Load("WireLight")).transform;
+		}
 	}
 	
 	void FixedUpdate() {
-		if (Time.time > nextSpawnTime) {
-			nextSpawnTime += spawnInterval;
-			Instantiate(transformToSpawn, transform.position, Quaternion.identity);
+		if (nextSpawnTime < Time.time) {
+			Debug.Log ("name: " + gameObject.name + " nextSpawnTime: " + nextSpawnTime + " time: " + Time.time + " interval: " + spawnInterval);
+			nextSpawnTime = Time.time + spawnInterval;
+			GameObject obj = ((Transform)Instantiate(transformToSpawn, transform.position, Quaternion.identity)).gameObject;
+			obj.GetComponent<FollowLinearPath>().setPath(pathName);
 		}
 	}
 }
