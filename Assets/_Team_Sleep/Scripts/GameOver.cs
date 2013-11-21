2c8d45938fa4478f4087c0705f1bb9c87d3e3299
Newman Willis <newmanwillis@gmail.com>
2013-11-21 00:19:07 -0600

    Game Over Screen adjusted to Boss Level

--- Assets/_Team_Sleep/Scripts/GameOver.cs
+++ Assets/_Team_Sleep/Scripts/GameOver.cs
@@ -8,9 +8,16 @@ public class GameOver : MonoBehaviour {
 	private Transform PlayerT;
 	private Transform BlackScreenCopy;
 	private tk2dSprite BlackScreenSprite;
+	private AudioListener AudioListen;
 	
 	// Use this for initialization
 	void Start () {
+
+		//transform.FindChild("Main Camera").GetComponents<AudioListener>().
+
+		AudioListen = transform.FindChild("Main Camera").GetComponent<AudioListener>();
+
+
 		//PlayerT =  GameObject.Find("Player").transform;
 		// BlackScreen.renderer.material.color = Color.clear;
 		//BlackScreen.Rotate(new Vector3(270,0,0));
@@ -36,10 +43,10 @@ public class GameOver : MonoBehaviour {
 		
 		// Move player forward in Z zone
 		Vector3 newPos = transform.position; // PlayerT.position;
-		newPos.z = -15;
+		newPos.z = -100;
 		BlackScreenCopy = ((Transform)(Instantiate(BlackScreen, newPos, Quaternion.identity)));
 		BlackScreenSprite = BlackScreenCopy.GetComponent<tk2dSprite>();
-		newPos.z = -50;
+		newPos.z = -200;
 		transform.position = newPos;
 		//PlayerT.position = newPos;	
 		// print("moved PlayerT: " + PlayerT.position);
@@ -54,10 +61,11 @@ public class GameOver : MonoBehaviour {
 	}
 	
 	IEnumerator HoldDeathScreen(){
-		
+		//AudioListen.enabled = false;
 		yield return new WaitForSeconds(6);
 		// print ("called reload on player");
 		GetComponent<Player>().ReloadPlayerAfterDeath();
+		//AudioListen.enabled = true;
 	}
 	
 }
